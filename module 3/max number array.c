d24d032310bc77383f1b1d204bba454dd843a15f
meet mulashiya <109717922+meetmulashiya@users.noreply.github.com>
2022-08-14 12:36:45 +0530

    Create max number array.c

---
+++ module 3/max number array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+
+int a[100], size,i,max;
+clrscr();
+printf("Enter size of arry:");
+scanf("%d", &size);
+printf("\nEnter value in arry:");
+for (i=0;i<size;i++)
+{
+
+    scanf("%d",&a[i]);
+
+}
+max=a[0];
+for(i=0;i<size;i++)
+{
+    if(a[i]>max)
+    {
+        max=a[i];
+    }
+}
+printf("Maximum value of Array: %d",max);
+getch();
