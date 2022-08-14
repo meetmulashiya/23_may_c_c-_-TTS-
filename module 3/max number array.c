924ba6b246589d7b701f416f4b444b386e06b857
meet mulashiya <109717922+meetmulashiya@users.noreply.github.com>
2022-08-14 12:52:33 +0530

    Update max number array.c

--- module 3/max number array.c
+++ module 3/max number array.c
@@ -1,25 +1,27 @@
 #include<stdio.h>
 #include<conio.h>
 void main()
-
-int a[100], size,i,max;
-clrscr();
-printf("Enter size of arry:");
-scanf("%d", &size);
-printf("\nEnter value in arry:");
-for (i=0;i<size;i++)
 {
 
-    scanf("%d",&a[i]);
-
-}
-max=a[0];
-for(i=0;i<size;i++)
-{
-    if(a[i]>max)
+    int a[100], size,i,max;
+    clrscr();
+    printf("Enter size of arry:");
+    scanf("%d", &size);
+    printf("\nEnter value in arry:");
+    for (i=0;i<size;i++)
     {
-        max=a[i];
+
+        scanf("%d",&a[i]);
+
     }
+    max=a[0];
+    for(i=0;i<size;i++)
+    {
+        if(a[i]>max)
+        {
+            max=a[i];
+        } 
+    }
+    printf("Maximum value of Array: %d",max);
+    getch();
 }
-printf("Maximum value of Array: %d",max);
-getch();
